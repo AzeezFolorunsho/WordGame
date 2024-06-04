2df147c1587f4a57c9b4f3d95d08b0d8d3a03cb2
Azeez Folorunsho <azeez-folorunsho@outlook.com>
2024-06-04 13:39:46 -0400

    Filled letter class

--- WordGameMain.py
+++ WordGameMain.py
@@ -59,18 +59,37 @@ current_letter_bg_x = 110
 indicators = []
 
 game_result = ""
+
 class Letter:
     def __init__(self, text, bg_position):
         # Initializes all the variables, including text, color, position, size, etc.
-        pass
+        self.bg_color = "white"
+        self.text_color = "black"
+        self.bg_position = bg_position
+        self.bg_x = self.bg_position[0]
+        self.bg_y = self.bg_position[1]
+        self.bg_rect = (bg_position[0], self.bg_y, LETTER_SIZE, LETTER_SIZE)
+        self.text = text
+        self.text_potition = (self.bg_x + 36, self.bg_position[1] + 34)
+        self.text_surface = GESSED_LETTER_FONT.render(self.text, True, self.text_color)
+        self.text_rect = self.text_surface.get_rect(center=self.text_potition)
  
     def draw(self):
         # Puts the letter and text on the screen at the desired positions.
-        pass
+        pygame.draw.rect(SCREEN, self.bg_color, self.bg_rect)
+
+        if self.bg_color == "white":
+            pygame.draw.rect(SCREEN, FILLED_OUTLINE, self.bg_rect, 3)
+
+        self.text_surface = GESSED_LETTER_FONT.render(self.text, True, self.text_color)
+        SCREEN.blit(self.text_surface, self.text_rect)
+        pygame.display.update()
  
     def delete(self):
         # Fills the letter's spot with the default square, emptying it.
-        pass
+        pygame.draw.rect(SCREEN, "white", self.bg_rect)
+        pygame.draw.rect(SCREEN, OUTLINE, self.bg_rect, 3)
+        pygame.display.update()
  
 class Indicator:
     def __init__(self, x, y, letter):
