e60c2a8b696f45c160c98a29a6220f72a5c325ae
Azeez Folorunsho <azeez-folorunsho@outlook.com>
2024-07-31 23:13:32 -0400

    Completed text box function

--- Funtions/text_box.py
+++ Funtions/text_box.py
@@ -1,33 +1,30 @@
 import pygame
 
 class Text_box:
-    def __init__(self, text, font, square_size, text_color, bg_color, outline_color, x, y, screen, filled_outline_color):
-        self.bg_color = bg_color
+    def __init__(self, text, font, square_size, text_color, bg_color, outline_color, x, y):
+        self.text = text
+        self.font = font
+        self.square_size = square_size
         self.text_color = text_color
+        self.bg_color = bg_color
+        self.outline_color = outline_color
         self.x = x
         self.y = y
         self.bg_rect = (self.x, self.y, square_size, square_size)
-        self.text = text
         self.text_position = (self.x + (square_size / 2), self.y + (square_size / 2))
-        self.text_surface = font.render(self.text, True, self.text_color)
+        self.text_surface = self.font.render(self.text, True, self.text_color)
         self.text_rect = self.text_surface.get_rect(center=self.text_position)
-        self.square_size = square_size
-        self.font = font
-        self.screen = screen
-        self.filled_outline_color = filled_outline_color
-        self.outline_color = outline_color
 
     # Puts the letter and text on the screen at the desired positions.
-    def draw(self):
+    def draw(self, screen):
+        self.screen = screen
         pygame.draw.rect(self.screen, self.bg_color, self.bg_rect)
-        if self.bg_color == "white":
-            pygame.draw.rect(self.screen, self.filled_outline_color, self.bg_rect, 3)
-        self.text_surface = self.font.render(self.text, True, self.text_color)
+        pygame.draw.rect(self.screen, self.outline_color, self.bg_rect, 3)
         self.screen.blit(self.text_surface, self.text_rect)
         pygame.display.update()
 
     # Fills the letter's spot with the default square, emptying it.
     def delete(self):
-        pygame.draw.rect(self.screen, "white", self.bg_rect)
+        pygame.draw.rect(self.screen, self.bg_color, self.bg_rect)
         pygame.draw.rect(self.screen, self.outline_color, self.bg_rect, 3)
         pygame.display.update()
