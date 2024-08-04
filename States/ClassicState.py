79fe5d8eefb6a16b932b9f9dcbca176f1389ae68
Advika R <artsytulip@gmail.com>
2024-08-03 22:50:27 -0500

    removed states folder

--- States/ClassicState.py
+++
@@ -1,9 +0,0 @@
-from .BaseState import BaseState
-from ..WorldeClasicMain import play_classic
-
-class ClassicState(BaseState):
-    def __init__(self):
-        super().__init__()
-
-    def handle_event(self, event):
-        play_classic()
