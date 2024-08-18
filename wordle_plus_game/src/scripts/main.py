d7d16a3061fccf1b93291eb5f6b364d37cf7f188
Sonu Gupta <sonu72752@gmail.com>
2024-08-18 18:06:50 -0500

    added settings button

--- wordle_plus_game/src/scripts/main.py
+++ wordle_plus_game/src/scripts/main.py
@@ -55,7 +55,8 @@ class Menu:
         self.buttons = [
             TextButton("Play Wordle Classic", self.button_font, (255, 255, 255), (0, 0, 0), (128, 128, 128), self.screen_width / 2 - 200, 250, 400, 50),
             TextButton("Play Wordle Hangman", self.button_font, (255, 255, 255), (0, 0, 0), (128, 128, 128), self.screen_width / 2 - 200, 350, 400, 50),
-            TextButton("Quit", self.button_font, (255, 255, 255), (0, 0, 0), (128, 128, 128), self.screen_width / 2 - 200, 450, 400, 50)
+            TextButton("Quit", self.button_font, (255, 255, 255), (0, 0, 0), (128, 128, 128), self.screen_width / 2 - 200, 450, 400, 50),
+            TextButton("Settings", self.button_font, (255, 255, 255), (0, 0, 0), (128, 128, 128), self.screen_width - (self.screen_width / 6), 30, 150, 50)
         ]
 
     def display_menu(self):
